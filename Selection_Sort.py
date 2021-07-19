33ac5ef32322609fcb3d4151e8f2783766e10d69
Suresh Gedela <75598983+SureshG13@users.noreply.github.com>
2021-07-19 16:22:42 +0530

    Update Selection_Sort.py

--- Selection_Sort.py
+++ Selection_Sort.py
@@ -1,1 +1,22 @@
 #Implementation of Selection Sort in Python
+#Finding minimum value from the left to right and replace with currect value
+
+l=[40,29,30,55,70,18,35]
+for i in range(len(l)-1):
+    for j in range(i+1,len(l)):
+        if l[j]<l[i]:
+            l[i],l[j]=l[j],l[i]
+print(l)
+
+
+
+#  Or you can do it in another way
+
+def selectionSort(l):
+  for i in range(len(l)-1):
+    for j in range(i+1,len(l)):
+      if l[j]<l[i]:
+        l[i],l[j]=l[j],l[i]
+  return l
+list1=[40,29,30,55,70,18,35]
+print(selectionSort(list1))
